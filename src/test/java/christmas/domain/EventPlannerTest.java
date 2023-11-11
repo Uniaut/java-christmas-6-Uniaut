32d91cf753332632cd063e47d3fa35beeae46b54
Uniaut <kunwoo0927@gmail.com>
2023-11-12 01:13:50 +0900

    test(domain.EventPlanner): add test of EventPlanner.setOrder()
    EventPlannerTest:
        fix test method name - setDateValid, setDateWithNull
        add test of setOrder() - valid value

--- src/test/java/christmas/domain/EventPlannerTest.java
+++ src/test/java/christmas/domain/EventPlannerTest.java
@@ -5,13 +5,15 @@ import static org.assertj.core.api.Assertions.assertThatNoException;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class EventPlannerTest {
     @DisplayName("setDate는 올바른 날짜를 입력받으면 예외가 발생하지 않는다.")
     @Test
-    void testEventPlannerConstructor() {
+    void setDateValid() {
         Calendar christmas = Calendar.getInstance();
         christmas.set(2023, Calendar.DECEMBER, 25);
         Date date = new Date(christmas.getTimeInMillis());
@@ -22,13 +24,27 @@ public class EventPlannerTest {
         );
     }
 
-    @DisplayName("이벤트 플래너 생성자는 Date로 null을 입력받으면 예외가 발생한다.")
+    @DisplayName("setDate는 null을 입력받으면 예외가 발생한다.")
     @Test
-    void testEventPlannerConstructorWithNull() {
+    void setDateWithNull() {
         EventPlanner eventPlanner = new EventPlanner();
 
         assertThatIllegalArgumentException().isThrownBy(
                 () -> eventPlanner.setDate(null)
         ).withMessageContaining("[ERROR]");
+    }
+
+    @DisplayName("이벤트 플래너 생성자")
+    @Test
+    void setOrderValid() {
+        Map<String, Integer> order = new HashMap<>();
+        order.put("타파스", 1);
+        order.put("제로콜라", 1);
+
+        EventPlanner eventPlanner = new EventPlanner();
+
+        assertThatNoException().isThrownBy(
+                () -> eventPlanner.setOrder(order)
+        );
     }
 }
