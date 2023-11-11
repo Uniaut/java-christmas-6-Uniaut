a2a85379c9b4b061e8a10181dc95f40d7376ca4e
Uniaut <kunwoo0927@gmail.com>
2023-11-12 01:34:34 +0900

    feature(domain.EventPlanner): implement EventPlanner.getTotalCost
    EventPlanner:
        implement getTotalCost()

--- src/main/java/christmas/domain/EventPlanner.java
+++ src/main/java/christmas/domain/EventPlanner.java
@@ -1,13 +1,19 @@
 package christmas.domain;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 public class EventPlanner {
     private Date date;
     private Map<String, Integer> order;
 
+    private final Map<String, Integer> menuCost;
+
     public EventPlanner() {
+        menuCost = new HashMap<>();
+        menuCost.put("타파스", 5500);
+        menuCost.put("제로콜라", 3000);
     }
 
     public void setDate(Date date) {
@@ -19,5 +25,11 @@ public class EventPlanner {
 
     public void setOrder(Map<String, Integer> order) {
         this.order = order;
+    }
+
+    public int getTotalCost() {
+        return order.keySet().stream()
+                .mapToInt(menu -> order.get(menu) * menuCost.get(menu))
+                .sum();
     }
 }
