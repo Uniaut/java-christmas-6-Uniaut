d57d8e03629a73566a5ebc18e03649a0c57e52b4
Uniaut <kunwoo0927@gmail.com>
2023-11-12 02:12:25 +0900

    feature(domain.EventPlanner): implement getWeekendBenefit
    EventPlanner:
        implement method - getWeekendBenefit
        add field - dessert

--- src/main/java/christmas/domain/EventPlanner.java
+++ src/main/java/christmas/domain/EventPlanner.java
@@ -2,13 +2,17 @@ package christmas.domain;
 
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class EventPlanner {
     private Calendar date;
     private Map<String, Integer> order;
 
     private final Map<String, Integer> menuCost;
+
+    private final List<String> dessert = List.of("초코케이크", "아이스크림");
 
     public EventPlanner() {
         menuCost = new HashMap<>();
@@ -31,6 +35,18 @@ public class EventPlanner {
         return order.keySet().stream()
                 .mapToInt(menu -> order.get(menu) * menuCost.get(menu))
                 .sum();
+    }
+
+    public int getWeekendBenefit() {
+        List<Integer> weekend = List.of(Calendar.SATURDAY, Calendar.SUNDAY);
+        if (weekend.contains(date.get(Calendar.DAY_OF_WEEK))) {
+            int numberDessert = order.entrySet().stream()
+                    .filter(menu -> dessert.contains(menu.getKey()))
+                    .mapToInt(Entry::getValue)
+                    .sum();
+            return numberDessert * 2023;
+        }
+        return 0;
     }
 
 }
