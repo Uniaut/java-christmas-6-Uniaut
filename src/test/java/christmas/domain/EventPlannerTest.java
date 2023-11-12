2af959aebb1405d0eaa244b97190d7ae0e7e2d55
Uniaut <kunwoo0927@gmail.com>
2023-11-13 01:37:31 +0900

    test(domain.EventPlanner): add test of EventPlanner
    EventPlannerTest:
        add test - getBenefitItems
        add test - getBenefitTotal
        add test - getDiscountTotalCost
        add test - getBadge

--- src/test/java/christmas/domain/EventPlannerTest.java
+++ src/test/java/christmas/domain/EventPlannerTest.java
@@ -5,8 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -78,5 +78,80 @@ public class EventPlannerTest {
 
         assertThat(eventPlanner.getWeekendBenefit())
                 .isEqualTo(2023 * 2);
+    }
+
+    // TODO: 최대한 많은 혜택 내역을 반영하도록 수정한다.
+    @DisplayName("getBenefitItems는 혜택 내역들을 반환한다.")
+    @Test
+    void getBenefitItems() {
+        Calendar dec30 = Calendar.getInstance();
+        dec30.set(2023, Calendar.DECEMBER, 30);
+
+        Map<String, Integer> order = new HashMap<>();
+        order.put("초코케이크", 2);
+        order.put("제로콜라", 1);
+
+        EventPlanner eventPlanner = new EventPlanner();
+        eventPlanner.setDate(dec30);
+        eventPlanner.setOrder(order);
+
+        assertThat(eventPlanner.getBenefitItems())
+                .isInstanceOf(List.class);
+
+    }
+
+    // TODO: 최대한 많은 혜택 내역을 반영하도록 수정한다.
+    @DisplayName("getBenefitTotal는 총혜택 금액을 반환한다. 증정을 포함한다.")
+    @Test
+    void getBenefitTotal() {
+        Calendar dec30 = Calendar.getInstance();
+        dec30.set(2023, Calendar.DECEMBER, 30);
+
+        Map<String, Integer> order = new HashMap<>();
+        order.put("초코케이크", 2);
+        order.put("티본스테이크", 2);
+
+        EventPlanner eventPlanner = new EventPlanner();
+        eventPlanner.setDate(dec30);
+        eventPlanner.setOrder(order);
+
+        assertThat(eventPlanner.getBenefitTotal())
+                .isEqualTo(2023 * 2 + 25000);
+    }
+
+    @DisplayName("getDiscountedTotalCost는 할인 후 예상 결제 금액을 반환한다. 증정은 제외한다.")
+    @Test
+    void getDiscountedTotalCost() {
+        Calendar dec30 = Calendar.getInstance();
+        dec30.set(2023, Calendar.DECEMBER, 30);
+
+        Map<String, Integer> order = new HashMap<>();
+        order.put("초코케이크", 2);
+        order.put("티본스테이크", 2);
+
+        EventPlanner eventPlanner = new EventPlanner();
+        eventPlanner.setDate(dec30);
+        eventPlanner.setOrder(order);
+
+        assertThat(eventPlanner.getDiscountedTotalCost())
+                .isEqualTo((55000 * 2) + (15000 * 2) - (2023 * 2));
+    }
+
+    @DisplayName("getBadge는 올바른 주문이 주어지면 해당하는 뱃지를 반환한다.")
+    @Test
+    void getBadge() {
+        Calendar dec30 = Calendar.getInstance();
+        dec30.set(2023, Calendar.DECEMBER, 30);
+
+        Map<String, Integer> order = new HashMap<>();
+        order.put("초코케이크", 2);
+        order.put("티본스테이크", 2);
+
+        EventPlanner eventPlanner = new EventPlanner();
+        eventPlanner.setDate(dec30);
+        eventPlanner.setOrder(order);
+
+        assertThat(eventPlanner.getBadge())
+                .isEqualTo("산타");
     }
 }
