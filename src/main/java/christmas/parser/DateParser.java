313152ee28622b2e5a502c6791a2da93000b247f
Uniaut <kunwoo0927@gmail.com>
2023-11-13 06:16:57 +0900

    feature(parser.DateParser): implement DateParser
    Parser:
        interface - parse()
    DateParser:
        implement parse

---
+++ src/main/java/christmas/parser/DateParser.java
@@ -0,0 +1,12 @@
+package christmas.parser;
+
+import java.util.Calendar;
+
+public class DateParser implements Parser<Calendar>{
+    public Calendar parse(String input) {
+        // TODO: add validation feature
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2023, Calendar.DECEMBER, Integer.parseInt(input));
+        return calendar;
+    }
+}
