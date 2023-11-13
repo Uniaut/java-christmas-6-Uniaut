ad514a3a2ed6e90b07247335d75812a202aafdae
Uniaut <kunwoo0927@gmail.com>
2023-11-14 01:08:54 +0900

    test(parser.DayDateParser): add test of DayDateParser
    DayDateParserTest:
        add test - invalid input
        add test - out of range input

--- src/test/java/christmas/parser/DayDateParserTest.java
+++ src/test/java/christmas/parser/DayDateParserTest.java
@@ -1,6 +1,7 @@
 package christmas.parser;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.util.Calendar;
 import org.junit.jupiter.api.DisplayName;
@@ -18,6 +19,30 @@ public class DayDateParserTest {
 
         assertThat(dayDateParser.parse(input))
                 .isEqualTo(actual);
+    }
+
+    @DisplayName("parse는 숫자가 아닌 값을 입력받으면 예외가 발생한다.")
+    @Test
+    void parseWithInvalidInput() {
+        DayDateParser dayDateParser = new DayDateParser();
+
+        String input = "a";
+
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> dayDateParser.parse(input)
+        ).withMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+    @DisplayName("parse는 1 ~ 31 범위 밖의 값을 입력받으면 예외가 발생한다.")
+    @Test
+    void parseWithOutOfRangeInput() {
+        DayDateParser dayDateParser = new DayDateParser();
+
+        String input = "32";
+
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> dayDateParser.parse(input)
+        ).withMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
 
 }
