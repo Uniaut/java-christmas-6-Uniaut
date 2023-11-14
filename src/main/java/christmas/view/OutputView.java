fe946c9d4405bd7f6c833b88c9b532fddbf55402
Uniaut <kunwoo0927@gmail.com>
2023-11-15 02:09:08 +0900

    feature(view.OuputView): add NONE condition print
    OutputView:
        print NONE if benefit is empty or 0

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -10,6 +10,8 @@ public class OutputView {
     private static final String TITLE_WELCOME = "우테코 식당 12월 이벤트 플래너입니다.";
     private static final String INTRO_DATE_FORMAT = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
 
+    private static final String NONE = "없음";
+
     private static final String TITLE_ORDER_ITEMS = "<주문 메뉴>";
     private static final String ORDER_ITEMS_FORMAT = "%s %d개";
 
@@ -60,6 +62,11 @@ public class OutputView {
 
     public void printFreebieItems(List<MenuItem> freebieItems) {
         ioInstance.print(TITLE_FREEBIE_ITEMS);
+        if (freebieItems.isEmpty()) {
+            ioInstance.print(NONE);
+            ioInstance.print("");
+            return;
+        }
         for (var item : freebieItems) {
             ioInstance.print(ORDER_ITEMS_FORMAT.formatted(item.name(), item.quantity()));
         }
@@ -68,6 +75,11 @@ public class OutputView {
 
     public void printBenefitItems(List<BenefitItem> benefitItems) {
         ioInstance.print(TITLE_BENEFIT_ITEMS);
+        if (benefitItems.isEmpty()) {
+            ioInstance.print(NONE);
+            ioInstance.print("");
+            return;
+        }
         for (var item : benefitItems) {
             ioInstance.print(BENEFIT_ITEMS_FORMAT.formatted(item.name(), item.amount()));
         }
@@ -76,6 +88,11 @@ public class OutputView {
 
     public void printTotalBenefit(int totalBenefit) {
         ioInstance.print(TITLE_TOTAL_BENEFIT);
+        if (totalBenefit == 0) {
+            ioInstance.print(NONE);
+            ioInstance.print("");
+            return;
+        }
         ioInstance.print(String.format(TOTAL_BENEFIT_FORMAT, totalBenefit));
         ioInstance.print("");
     }
